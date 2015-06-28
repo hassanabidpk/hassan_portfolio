d6489a8581e67f0caf5fb43c3e5ace9aaeb06f1f
Hassan Abid <hassanabidpk89@gmail.com>
2015-06-29 00:07:25 +0900

    add cdn links

--- hassan_portfolio/wsgi.py
+++ hassan_portfolio/wsgi.py
@@ -15,6 +15,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hassan_portfolio.settings")
 
 # application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = get_wsgi_application()
