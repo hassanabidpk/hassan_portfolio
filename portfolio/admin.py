c80551592eda97718d73cd8e11dd00f429366ee8
Hassan Abid <hassanabidpk89@gmail.com>
2015-06-29 23:57:58 +0900

    add edit feature

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -21,6 +21,7 @@ class BlogAdmin(admin.ModelAdmin):
 	('Date Information',{ 'fields': ['post_published'], 'classes': ['collapse']}),]
 
 	list_display = ('post_author', 'post_title', 'post_content','post_photo','post_category','post_tags','post_published','post_modified')
+	list_editable = ('post_title', 'post_content','post_photo','post_category','post_tags')
 	list_filter = ['post_modified','post_published']
 	search_fields = ['post_title', 'post_content']
 
